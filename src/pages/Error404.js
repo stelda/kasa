1cbd40453295acd4b07ee12db1d60bc956f8ac41
Cristel <stelda@outlook.fr>
2024-05-17 15:15:27 +0200

    Complete Error404 page

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -3,9 +3,10 @@ import React from 'react';
 function Error404() {
     return (
         <div>
-            <div>
-                <h1>Error 404</h1>
-                <p>Page not found.</p>
+            <div className="error404">
+                <h1>404</h1>
+                <p>Oups! La page que vous demandez n'existe pas.</p>
+                <a href="/">Retourner sur la page d'accueil</a>
             </div>
         </div>
     );
