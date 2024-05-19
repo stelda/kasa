8e0a303477f241d7c941940ad731d576547d7501
Cristel <stelda@outlook.fr>
2024-05-19 14:31:14 +0200

    Start Housing pages

---
+++ src/components/Dropdown.js
@@ -0,0 +1,33 @@
+import React, {useState} from "react";
+
+function Dropdown({ title, content }) {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleDropdown = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <div className="dropdown">
+            <div className="dropdown-header" onClick={toggleDropdown}>
+                <h3>{title}</h3>
+                <span className={`dropdown-arrow ${isOpen ? 'open' : ''}`}>&#9660;</span>
+            </div>
+            {isOpen && (
+                <div className="dropdown-content">
+                    {Array.isArray(content) ? (
+                        <ul>
+                            {content.map((item, index) => (
+                                <li key={index}>{item}</li>
+                            ))}
+                        </ul>
+                    ) : (
+                        <p>{content}</p>
+                    )}
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default Dropdown;
--- src/pages/About.js
+++ src/pages/About.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from "../components/Banner";
 import aboutBannerImage from "../assets/about_banner.jpg";
 
+
 function About() {
     return (
         <main className='main-container'>
@@ -11,10 +12,6 @@ function About() {
                 className="about-banner"
             />
 
-            <div className='about-content'>
-                <h1>About Us</h1>
-                <p>Learn more about our company.</p>
-            </div>
         </main>
     );
 }
