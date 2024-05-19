7e696b2fe3113407f6836eaa5b636fb76a3d4d2e
Cristel <stelda@outlook.fr>
2024-05-19 17:00:18 +0200

    Complete About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Banner from "../components/Banner";
 import aboutBannerImage from "../assets/about_banner.jpg";
+import Dropdown from "../components/Dropdown";
+import about_content from "../data/about_content.js";
 
 
 function About() {
@@ -11,6 +13,12 @@ function About() {
                 backgroundImage={aboutBannerImage}
                 className="about-banner"
             />
+
+            <section className="about-content">
+                {about_content.map((item, index) => (
+                    <Dropdown key={index} title={item.title} content={item.content} />
+                ))}
+            </section>
 
         </main>
     );
